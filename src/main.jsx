a9844817cc6843b55ddea7a2bc154ecf1e1dfb5b
Johathan29 <rosariojohathan@gmail.com>
2025-06-26 15:57:36 -0400

    react-nw

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-
+import HeaderComponent from './Components/HeaderComponent.jsx';
+import FooterComponent from './Components/HeaderComponent.jsx';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HeaderComponent/>
     <App />
+    <FooterComponent/>
   </StrictMode>
 );

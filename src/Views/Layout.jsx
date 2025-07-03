102098c9b606ec1dc077b33034d55b7d5042f75f
Johathan29 <84540634+Johathan29@users.noreply.github.com>
2025-07-03 19:25:18 -0400

    Update Layout.jsx

--- src/Views/Layout.jsx
+++ src/Views/Layout.jsx
@@ -5,13 +5,10 @@ export default function Layout(){
     return(
         <>
          <HeaderComponent></HeaderComponent>
-<<<<<<< HEAD
-         <div className="max-w-screen-xl md:mx-auto py-[2rem]">
-            <div className="h-full min-h-[28rem] ">
-=======
+
          <div className="max-w-screen-xl mx-auto py-[2rem]">
             <div className="h-[40rem] ">
->>>>>>> 16d950ea618718884b2a6b9924ee3c6247e51ad6
+
             <Outlet />
             </div>
         </div>

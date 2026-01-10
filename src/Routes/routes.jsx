98350fac382b925fdf2258a6a1e191cf4f3780ff
Johathan Rosario <84540634+Johathan29@users.noreply.github.com>
2026-01-10 00:38:41 -0400

    Add files via upload

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -25,7 +25,7 @@ import ProductsDashboard from "../Views/ProductsDashboard.jsx";
 import ProtectedRoute from "./ProtectedRoute.jsx";
 import Login from "../Components/FormLogin.jsx";
 import Register from "../Components/Register.jsx";
-import Carts from "../Views/carts.jsx";
+import Carts from "../Views/Carts.jsx";
 import AdminRolesPanel from "../Views/gestionUser.jsx";
 import About from "../Views/About.jsx";
 import FormUpdateUsers from "../Views/FormUpdateUser.jsx";
